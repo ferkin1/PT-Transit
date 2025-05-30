cb2619aa52fa921d5cb99cfc91cffbf122768a44
Tleax <tamatoa.ynam@gmail.com>
2025-05-29 19:29:51 -1000

    - Add the Header.vue

---
+++ frontend/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Header">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/testing">Testing 1</NuxtLink>
+    <NuxtLink to="/testing2">Testing 2</NuxtLink>
+  </div>
+</template>
+<style lang="css">
+
+.Header > a {
+  margin-right: 0.5rem;
+}
+</style>
