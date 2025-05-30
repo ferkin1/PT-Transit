a4540d3cfaa73568af539067175faf98b0c1ad6f
Ferkin <ferkintwitch@gmail.com>
2025-05-29 21:25:51 -0700

    - reorganized frontend dir

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        // any request to /api/* will be forwarded to your FastAPI
+        '/api/': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
+      },
+    },
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui']
+  modules: ['@nuxt/ui', '@nuxt/image']
 })
