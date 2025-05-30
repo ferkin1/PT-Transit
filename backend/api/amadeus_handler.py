0eac049ce7a982392c82b9cfe9fab62fab590136
Ferkin <ferkintwitch@gmail.com>
2025-05-29 18:41:08 -0700

    - Added some type hinting to amadeus_handler.py

--- backend/api/amadeus_handler.py
+++ backend/api/amadeus_handler.py
@@ -1,18 +1,20 @@
 import os
 from amadeus import Client, ResponseError, Location
 import requests
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class AmadeusHandler:
-
     def __init__(self):
-        self.am_client_id = os.getenv("AMADEUS_API")
-        self.am_client_secret = os.getenv("AMADEUS_SECRET")
+        self.am_client_id:str = os.getenv("AMADEUS_API")
+        self.am_client_secret:str = os.getenv("AMADEUS_SECRET")
         self.client = Client(client_id=self.am_client_id,
                              client_secret=self.am_client_secret)
+
+        #self.client.next(response)  # => returns a new response for the next page
 
     def __enter__(self):
         return self
@@ -40,7 +42,16 @@ class AmadeusHandler:
 
 if __name__ == "__main__":
     with AmadeusHandler() as api:
-        testing = api.client.reference_data.locations.get(keyword='LON',
-                                                          subType=Location.AIRPORT)
-
-    print(testing.data)
+        # testing = api.client.reference_data.locations.get(keyword='LON',
+        #                                                   subType=Location.AIRPORT)
+        # testing = api.client.shopping.flight_destinations.get(origin='MAD')
+        params = {"originCityCode": "MAD",
+                  "period": "2017-01",
+                  "max": 50,
+                  "page[limit]": 50}
+        testing = api.get_any("/v1/travel/analytics/air-traffic/traveled", **params)
+        print(testing)
+        # data = testing.data
+        # df = pd.DataFrame(testing)
+        # df.to_csv("test_flights_most_traveled.csv", index=False)
+        # print(df)
