1a65bb4979109843359c13cce58f85b55a8152b3
Tleax <tamatoa.ynam@gmail.com>
2025-05-29 16:46:17 -1000

    update file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
       proxy: {
         // any request to /api/* will be forwarded to your FastAPI
         '/api/': {
-          target: 'http://localhost:3000',
+          target: 'http://localhost:8000',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '/api'),
         },
